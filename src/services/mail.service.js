71ec027b5843b12f070c522989013f153625234b
nicohermida01 <nshermida101@gmail.com>
2023-08-14 15:16:17 -0300

    change email name

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -7,9 +7,9 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 
 const sendEmail = async input => {
 	const data = await resend.emails.send({
-		from: 'Acme <noreply@nicohermida.com>',
+		from: 'Portfolio <noreply@nicohermida.com>',
 		to: ['hermida.nicolas101@gmail.com'],
-		subject: `Portfolio | New message from: ${input.name}`,
+		subject: `New message from: ${input.fromName}`,
 		react: EmailTemplate({
 			name: input.fromName,
 			mail: input.fromEmail,
